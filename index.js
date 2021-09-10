72300f209ab0d4ffd612d2ba466c649ae368838b
Lena Hou <lenahou2013@gmail.com>
2021-09-10 14:49:25 -0400

    commit tree 0e5b94d663a460233551c09decb313d123f56032

--- index.js
+++ index.js
@@ -10,4 +10,26 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time) {
+  const hourString = time.split(":");
+  const hourInt = parseInt(hourString, 10);
+  
+  if (hourInt < 12) {
+  return displayMessage("Good Morning");
+  }
+
+  else if (hourInt >= 12 && hourInt <= 17) {
+    return displayMessage("Good Afternoon");
+  }
+
+  else {
+    return displayMessage("Good Evening");
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  const element = document.getElementById("greeting");
+  element.innerText = message;
+  return message;
+}
